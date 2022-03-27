2c53d6973c3b5639fbf0dafcaa95bf83c533f4d8
iyume <iyumelive@gmail.com>
2022-03-27 13:05:21 +0800

    fix: reexport

--- nonebot_plugin_params/__init__.py
+++ nonebot_plugin_params/__init__.py
@@ -2,4 +2,4 @@ from .deps import AdapterName as AdapterName
 from .deps import EventName as EventName
 from .deps import ImageSegmentMethod as ImageSegmentMethod
 from .deps import MessageSegmentClass as MessageSegmentClass
-from .permission import PRIVATEMESSAGE
+from .permission import PRIVATEMESSAGE as PRIVATEMESSAGE
